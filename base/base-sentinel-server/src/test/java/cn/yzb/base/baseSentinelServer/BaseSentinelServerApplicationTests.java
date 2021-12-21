707a18efbcdfa63730a27f42c3e6b41820db96ee
yangzhebin <289885796@qq.com>
2021-12-21 15:06:24 +0800

    nacos服务的发现和注册

---
+++ base/base-sentinel-server/src/test/java/cn/yzb/base/baseSentinelServer/BaseSentinelServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.yzb.base.baseSentinelServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseSentinelServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
