1b490138fbfe99479888bdf2cf6fc8e63ba77f82
yangzhebin <289885796@qq.com>
2021-12-21 11:06:24 +0800

    第一次提交

---
+++ base/base-nacos-server/src/main/java/cn/yzb/base/baseNacosServer/BaseNacosServerApplication.java
@@ -0,0 +1,13 @@
+package cn.yzb.base.baseNacosServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseNacosServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseNacosServerApplication.class, args);
+    }
+
+}
---
+++ base/base-nacos-server/src/test/java/cn/yzb/base/baseNacosServer/BaseNacosServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.yzb.base.baseNacosServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseNacosServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
