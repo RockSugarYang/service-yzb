7014773b4aeb939c6c6582760960d48a4c75477c
yangzhebin <289885796@qq.com>
2021-12-22 17:19:31 +0800

    nacos配置中心测试

---
+++ base/base-config-server/src/main/java/cn/yzb/base/BaseConfigServer/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package cn.yzb.base.BaseConfigServer.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yzb
+ * @description:
+ * @date 2021/12/22 16:11
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
